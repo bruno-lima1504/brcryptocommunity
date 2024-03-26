e83b11af02f06fcba75278b0b0da4ec11516231f
Bruno Lima <102754701+bruno-lima1504@users.noreply.github.com>
2024-03-26 17:50:01 +0000

    adiciona estilo a pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import Head from "next/head";
+import styles from "../styles/Construcao.module.css";
+
 function Home() {
-  return <h1>Página Inicial de teste</h1>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Em Construção</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Esta página está em construção</h1>
+        <p>
+          Desculpe pelo transtorno, estamos trabalhando nisso. Por favor, volte
+          mais tarde!
+        </p>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
