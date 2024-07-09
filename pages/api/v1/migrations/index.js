32e8503e33501cccc7cd2cc658c01ab019c432c6
bruno-lima1504 <bruno.lima1504@gmail.com>
2024-07-09 20:41:16 -0300

    add: `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,13 +1,37 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function teste(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      response.status(201).json(migratedMigrations);
+    }
+    response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end;
 }
